b42f367b94a68bf81bad52aa18e52b8d25a0ca3f
김민성 <minseong@gimminseong-ui-MacBookPro.local>
2021-02-27 22:38:03 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c7e8e3fa967e1ce63240.js"
+    "url": "webpack-runtime-c258e70fca98c46e1685.js"
   },
   {
     "url": "styles.08b6f1b92a40a3f4c705.css"
@@ -42,11 +42,11 @@ self.__precacheManifest = [
     "url": "532a2f07-a2b9837034ae8adfec32.js"
   },
   {
-    "url": "app-347fa43aafa96dd771d9.js"
+    "url": "app-3ecd476b23cfc5503d23.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3946203eaaf1432a6ec5a58d8e845fa3"
+    "revision": "01ca94afb21e40ebf5fb760e75aa16c9"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5081bfd3b3053fb653b2.js"
@@ -60,7 +60,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "69f72adbc7300f12211dbe28af5fce7d"
+    "revision": "ea71769a5d9c12c5612a6e256bb50e9e"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -152,7 +152,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-347fa43aafa96dd771d9.js`))) {
+  if (!resources || !(await caches.match(`/app-3ecd476b23cfc5503d23.js`))) {
     return await fetch(event.request)
   }
 
